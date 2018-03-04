2e70a57c9d2d1e1e857f273b918780ae7b191a28
鲍明 <1934128057@qq.com>
2018-03-04 18:22:37 +0800

    first commit

---
+++ location/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  location
+//
+//  Created by 鲍明 on 2018/3/4.
+//  Copyright © 2018年 test. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ location/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  location
+//
+//  Created by 鲍明 on 2018/3/4.
+//  Copyright © 2018年 test. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
